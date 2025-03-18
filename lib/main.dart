0875ca8b5718185a1d9a7e75268d81faa03f0e37
ChenGuoXiang940 <s1411232069@ad1.nutc.edu.tw>
2025-03-18 23:04:01 +0800

    新增

--- lib/main.dart
+++ lib/main.dart
@@ -50,8 +50,11 @@ class Data {
   /// 商品數量
   int number;
 
+  /// 購買者
+  String? name;
+
   /// 建構函式，初始化商品名稱、價格和數量
-  Data(this.item, this.price, this.number);
+  Data(this.item, this.price, this.number,this.name);
 }
 
 class MyHomePageState extends State<MyHomePage> {
@@ -59,6 +62,7 @@ class MyHomePageState extends State<MyHomePage> {
   final TEC _itemC = TEC();
   final TEC _priceC = TEC();
   final TEC _numberC = TEC();
+  String? _username;
 
   /// 顯示訂單明細的對話框
   void _allow() {
@@ -66,14 +70,15 @@ class MyHomePageState extends State<MyHomePage> {
       context: context,
       builder: (BuildContext context) {
         double total = 0;
-        for (var cur in col) {
+        final userOrders = col.where((data) => data.name == _username).toList();
+        for (var cur in userOrders) {
           total += cur.price * cur.number;
         }
         return AlertDialog(
           title: const Text('訂單明細'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
-            children: col.map((x) {
+            children: userOrders.map((x) {
               return ListTile(
                 title: Text(x.item),
                 subtitle: Text('\$${x.price} x ${x.number}'),
@@ -110,8 +115,12 @@ class MyHomePageState extends State<MyHomePage> {
       _showErrorDialog("輸入格式錯誤");
       return;
     }
+    if(_username==null){
+      _showErrorDialog("請先登入");
+      return;
+    }
     setState(() {
-      col.add(Data(item, price, number));
+      col.add(Data(item, price, number,_username));
       _itemC.clear();
       _priceC.clear();
       _numberC.clear();
@@ -119,9 +128,9 @@ class MyHomePageState extends State<MyHomePage> {
   }
 
   /// 移除指定的 ListTile
-  void _remove(int index) {
+  void _remove(Data data) {
     setState(() {
-      col.removeAt(index);
+      col.remove(data);
     });
   }
 
@@ -158,6 +167,11 @@ class MyHomePageState extends State<MyHomePage> {
         child: Center(
           child: Column(
             children: <Widget>[
+              if(_username!=null)
+                Text('歡迎登入, $_username',
+                style: TextStyle(fontSize: 20, color: Colors.green)
+                )
+              ,
               TextField(
                 controller: _itemC,
                 decoration: const InputDecoration(labelText: "商品"),
@@ -175,11 +189,27 @@ class MyHomePageState extends State<MyHomePage> {
                 onPressed: _addOrder,
                 child: const Text('加入購物車'),
               ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  // 導航到第二頁並等待結果
+                  final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SecondPage(title: "登入頁面")),
+                  );
+                  if (result != null) {
+                    setState(() {
+                      _username = result;
+                    });
+                  }
+                },
+                child: const Text('前往登入'),
+              ),
               Expanded(
                 child: ListView.builder(
-                  itemCount: col.length,
+                  itemCount: col.where((data) => data.name == _username).length,
                   itemBuilder: (context, index) {
-                    final cur = col[index];
+                    final cur = col.where((data) => data.name == _username).toList()[index];
                     return ListTile(
                       title: Text(cur.item, style: TextStyle(color: Colors.deepPurpleAccent)),
                       subtitle: Text('\$${cur.price} x ${cur.number}'),
@@ -187,15 +217,16 @@ class MyHomePageState extends State<MyHomePage> {
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           Text('總共: ${cur.price * cur.number}'),
-                          IconButton( 
-                            onPressed: () => _remove(index),
-                            icon: const Icon(Icons.remove), ),
+                          IconButton(
+                            onPressed: () => _remove(cur),
+                            icon: const Icon(Icons.remove),
+                          ),
                         ],
                       ),
                     );
                   },
                 ),
-              ),
+              )
             ],
           ),
         ),
@@ -204,6 +235,93 @@ class MyHomePageState extends State<MyHomePage> {
         onPressed: _allow,
         tooltip: 'Check',
         child: const Icon(Icons.check),
+      ),
+    );
+  }
+}
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key, required this.title});
+  final String title;
+
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  final TEC _name = TEC();
+  final TEC _password = TEC();
+  String? _msg = "";
+
+  void _logout() {
+    String? name_text = _name.text;
+    String? password = _password.text;
+
+    if (name_text.isEmpty || password.isEmpty) {
+      setState(() {
+        _msg = "登入失敗,請重試輸入";
+      });
+      return;
+    }
+    Navigator.pop(context, _name.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('登入', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurpleAccent,
+        foregroundColor: Colors.white,
+      ),
+      backgroundColor: Colors.white, // Set the background color to white
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "資工購物平台",
+                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _name,
+                decoration: const InputDecoration(
+                  labelText: "用戶名",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _password,
+                decoration: const InputDecoration(
+                  labelText: "密碼",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _logout,
+                style: ElevatedButton.styleFrom(
+                  iconColor: Colors.deepPurpleAccent,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: const TextStyle(fontSize: 18),
+                ),
+                child: const Text('登入'),
+              ),
+              const SizedBox(height: 20),
+              if (_msg != null)
+                Text(
+                  '$_msg',
+                  style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
